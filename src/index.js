48d51c1e45e8fe018102bc572a76a108a590cd2e
artem-yaremchuk <119439325+artem-yaremchuk@users.noreply.github.com>
2023-10-12 15:23:00 +0300

    update#9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import Notiflix from "notiflix";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import { serviceImages } from "./service-images.js";
 
 const refs = {
